33875a8c37b55d6ba7d4ade3c69aa8c366eec8b1
Ayush <ayushkhadkak@gmail.com>
2026-02-19 16:15:30 +0100

    automation of opening msteams

---
+++ microsoftteams.py
@@ -0,0 +1,53 @@
+import time
+import subprocess
+import pyautogui
+import pygetwindow as gw
+
+pyautogui.FAILSAFE = True
+
+
+def openMicroTeams():
+    subprocess.Popen("start ms-teams:", shell=True)
+    print("im open")
+
+
+def useMicroTeamsApp():
+
+    for _ in range(60):
+        winsobj = gw.getWindowsWithTitle("Microsoft Teams")
+        if winsobj:
+            win = winsobj[0]
+
+            if win.isMinimized:
+                win.restore()
+                time.sleep(1)
+
+            x, y = win.center
+            pyautogui.click(x, y)
+
+            time.sleep(0.5)
+            return
+
+        time.sleep(0.5)
+
+
+def navigateToMeet():
+    pyautogui.hotkey("ctrl", "1")
+    time.sleep(1)
+
+    pyautogui.hotkey("ctrl", "2")
+    time.sleep(1)
+
+    pyautogui.press("tab", presses=4, interval=0.4)
+    pyautogui.press("right", presses=2, interval=0.4)
+    pyautogui.press("enter")
+    pyautogui.press("tab", presses=8, interval=0.4)
+    pyautogui.press("enter")
+
+openMicroTeams()
+time.sleep(5)
+useMicroTeamsApp()
+navigateToMeet()
+
+print("made it till here")
+
